304303c25f7459c4a0c2877f00af9c9c1cd137e8
palaciossegundo <98151018+palaciossegundo@users.noreply.github.com>
2022-03-11 10:14:48 +0100

    gg

---
+++ PalaciosVettori1/PalaciosVettori1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PalaciosVettori1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 400;
+            
+            int[] vett = new int[dimensione];
+            Random generator = new Random();
+             for (int i = 0; i < dimensione; i++)
+             {
+                vett[i] = generator.Next(0, 101);
+                Console.WriteLine($"vett[{i}]={vett[i]}");
+             }
+            
+        }
+    }
+}
