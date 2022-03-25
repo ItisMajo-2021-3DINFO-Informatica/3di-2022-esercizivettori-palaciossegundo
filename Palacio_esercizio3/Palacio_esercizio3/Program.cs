b2d4232e235f83c3c7cf732b3c8a558b2570cbce
palaciossegundo <98151018+palaciossegundo@users.noreply.github.com>
2022-03-25 09:32:49 +0100

    esercizio3 new new

---
+++ Palacio_esercizio3/Palacio_esercizio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Palacio_esercizio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 30;
+            int[] numeri = new int[dimensione];
+            int somma = 0;
+            Random generatore = new Random();
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                numeri[i] = generatore.Next(0, 51);
+                Console.WriteLine($"vett[{i}]={numeri[i]}");
+                somma = somma + numeri[i];
+            }
+            int media = somma / 30;
+            Console.Write($"La media è = {media}");
+        }
+    }
+}
