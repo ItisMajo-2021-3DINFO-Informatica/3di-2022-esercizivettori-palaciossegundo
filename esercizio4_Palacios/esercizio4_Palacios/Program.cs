83b80b4b1f13a0d3ed29619a1f705919e08cd53a
palaciossegundo <98151018+palaciossegundo@users.noreply.github.com>
2022-03-25 10:13:48 +0100

    gg4

---
+++ esercizio4_Palacios/esercizio4_Palacios/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace esercizio4_Palacios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 100;
+            int[] numeri = new int[dimensione];
+            Random generatore = new Random();
+            int min = 4000;
+            int max = 0;
+           
+            for (int i = 0; i < dimensione; i++)
+            {
+                numeri[i] = generatore.Next(1000, 3000);
+                Console.WriteLine($"vett[{i}]={numeri[i]}");
+                
+            }
+            for(int i = 0; i < dimensione; i++)
+            {
+                if (numeri[i] < min)
+                {
+                    min = numeri[i];
+                }
+                if (numeri[i] > max)
+
+                {
+                    max = numeri[i];
+
+                }
+            }
+
+            Console.WriteLine("Il numero piu piccolo è: " + min );
+            Console.WriteLine("Il numero piu grande è: " + max);
+
+        }
+    }
+}
